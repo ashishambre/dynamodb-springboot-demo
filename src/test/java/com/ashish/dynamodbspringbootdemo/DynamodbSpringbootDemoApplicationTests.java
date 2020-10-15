33c56187d55ad70f8c861d9c560f71f33d4369a9
Aashish Ambre <a.ambre06@gmail.com>
2020-10-15 20:00:51 +0530

    Initial Commit

---
+++ src/test/java/com/ashish/dynamodbspringbootdemo/DynamodbSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.dynamodbspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
